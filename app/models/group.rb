4d8f838bf50ce223a777bc8b984a8782f2ad1e6d
Vladimir <ostapenkov74@gmail.com>
2025-06-23 12:23:48 +0500

    Add group

---
+++ app/models/group.rb
@@ -0,0 +1,12 @@
+class Group < ApplicationRecord
+  as_many :group_memberships
+  has_many :employees, through: :group_memberships
+
+  has_many :reporting_relationships_as_supervisor,
+           as: :supervisor,
+           class_name: 'ReportingRelationship'
+
+  has_many :subordinates,
+           through: :reporting_relationships_as_supervisor,
+           source: :subordinate
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_23_071848) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_23_072222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
